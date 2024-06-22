81a21a8c6a85c697e0deac70d0044d4cd65af7db
jadg13 <jadg13@hotmail.com>
2024-06-21 19:34:42 -0600

    update 21/6/24

--- EjemploMenu/app/src/main/java/online/jadg13/ejemplomenu/Suma.kt
+++ EjemploMenu/app/src/main/java/online/jadg13/ejemplomenu/Suma.kt
@@ -5,55 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import online.jadg13.ejemplomenu.databinding.FragmentSumaBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Suma.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Suma : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding : FragmentSumaBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_suma, container, false)
+        binding = FragmentSumaBinding.inflate(layoutInflater)
+
+        start()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Suma.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Suma().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    public fun start(){
+        binding.BtnSuma.setOnClickListener {
+            val num1:Int = binding.TfNum1.editText?.text.toString().toInt()
+            val num2:Int = binding.TfNum2.editText?.text.toString().toInt()
+            val resultado:Int = suma(num1, num2)
+            binding.TvResultado.text = resultado.toString()
+        }
     }
+
+    private fun suma(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+
 }
